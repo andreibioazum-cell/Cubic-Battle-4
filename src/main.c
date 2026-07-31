b884def296af678a8889dcb16db02790e35ef742
andreibioazum-cell <andreibioazum@gmail.com>
2026-07-31 14:03:24 +0400

    Update src/main.c

--- src/main.c
+++ src/main.c
@@ -1,44 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <SDL2/SDL.h>
-int main(int argc, char *argv[]) {
-SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK);
-SDL_Window *window = SDL_CreateWindow("Cubic Battle 4", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
-SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-SDL_Joystick *joystick = NULL;
-if (SDL_NumJoysticks() > 0) {
-joystick = SDL_JoystickOpen(0);
-}
-int fps = 60;
-Uint32 last_time = SDL_GetTicks();
-while (1) {
-SDL_Event event;
-while (SDL_PollEvent(&event)) {
-if (event.type == SDL_QUIT) {
-goto cleanup;
-}
-if (event.type == SDL_JOYBUTTONDOWN || event.type == SDL_JOYAXISMOTION) {
-// Обработка событий джойстика
-if (joystick) {
-int x = SDL_JoystickGetAxis(joystick, 0);
-int y = SDL_JoystickGetAxis(joystick, 1);
-printf("Joystick: x=%d, y=%d\n", x, y);
-}
-}
-}
-Uint32 current_time = SDL_GetTicks();
-float delta_time = (current_time - last_time) / 1000.0f;
-last_time = current_time;
-int rounded_fps = (int)(1.0f / delta_time + 0.5f);
+// Использование округления FPS
+#include "game.h"
+int main() {
+// Инициализация игры
+Game game;
+init_game(&game);
+float target_fps = 60.0f;
+float dt = 1.0f / target_fps;
+while (game_is_running(&game)) {
+float current_time = get_current_time();
+float elapsed_time = current_time - game.last_frame_time;
+game.last_frame_time = current_time;
+// Округление FPS
+int rounded_fps = round_fps(1.0f / elapsed_time);
 printf("FPS: %d\n", rounded_fps);
-// Остальной код игры...
+update_player_movement(&game.player, dt);
+update_game(&game, dt);
+render_game(&game);
 }
-cleanup:
-if (joystick) {
-SDL_JoystickClose(joystick);
-}
-SDL_DestroyRenderer(renderer);
-SDL_DestroyWindow(window);
-SDL_Quit();
+destroy_game(&game);
 return 0;
 }
